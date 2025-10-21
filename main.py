fa035c66fb5bf98723d0136d3d6517574e8f188e
Kiwi8474 <uenalmaximilian@proton.me>
2025-10-21 21:46:16 +0200

    feat: add voice chat rewards and changing currency courses

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from dotenv import load_dotenv
 import json
 import os
 import random
+import asyncio
+import time
 
 # --- Hinweise/Erinnerungen ---
 
@@ -42,17 +44,31 @@ CURRENCIES_DISPLAY = {
 BASE_CURRENCY_NAME = "aetherium"
 BASE_CURRENCY_VALUE = 1.0
 VOLATILE_CURRENCIES = ["celesti", "solari", "glimmer"]
+IDEAL_SUPPLY = 100000
+INFLATION_EXPONENT = 0.1
+UPDATE_INTERVAL = 1 * 60 * 60 # Intervall für die Währungsupdates. 1 Stunde in Sekunden
+
+# Voice Belohnungen
+VOICE_REWARD_INTERVAL = 1 * 60
+AETHERIUM_REWARD_PER_VOICE_INTERVAL = 0.01666
 
 
 # --- Dateien ---
 
 files = [
-    "pot", "user_currencies", "currency_courses"
+    "pot", "user_currencies", "currency_courses",
+    "currency_totals"
 ]
 
+
+# --- Dictionaries ---
+
 dicts = {
-    "pot": {}, "user_currencies": {}, "currency_courses": {}
+    "pot": {}, "user_currencies": {}, "currency_courses": {},
+    "currency_totals": {}
 }
+
+voice_start_times = {}
 
 
 # --- Hilfsfunktionen ---
@@ -67,6 +83,66 @@ def save_all_files():
         with open(f"{dic}.json", "w") as f:
             json.dump(dicts[dic], f, indent=4)
 
+def calculate_currencies(current_courses, currency_totals):
+    new_courses = current_courses.copy()
+    MAX_CHANGE_PERCENT = 1
+
+    for currency in VOLATILE_CURRENCIES:
+        current_value = new_courses[currency]
+
+        current_supply = currency_totals[currency]
+        supply_ratio = current_supply / IDEAL_SUPPLY
+        inflation_factor = supply_ratio ** INFLATION_EXPONENT
+
+        max_abs_change = current_value * MAX_CHANGE_PERCENT
+        change_amount = random.uniform(-max_abs_change, max_abs_change)
+
+        final_change = change_amount / inflation_factor
+        new_value = current_value + final_change
+    
+    return new_courses
+
+def update_currency_courses():
+    dicts["currency_courses"] = calculate_currencies(dicts["currency_courses"], dicts["currency_totals"])
+    save_all_files()
+
+
+# --- Loops ---
+
+async def currency_update_loop():
+    await bot.wait_until_ready()
+    while not bot.is_closed():
+        update_currency_courses()
+        await asyncio.sleep(UPDATE_INTERVAL)
+
+async def reward_voice_loop():
+    await bot.wait_until_ready()
+
+    heavenly_area = bot.get_guild(HEAVENLY_AREA_ID)
+
+    while not bot.is_closed():
+        await asyncio.sleep(VOICE_REWARD_INTERVAL)
+
+        celesti_to_aetherium = dicts["currency_courses"]["celesti"]
+
+        for member in heavenly_area.members:
+            if member.voice and member.voice.channel and not member.bot:
+
+                # wenn der user stumm/taub ist, überspringen
+                if member.voice.self_mute or member.voice.self_deaf:
+                    continue
+
+                celesti_reward = AETHERIUM_REWARD_PER_VOICE_INTERVAL / celesti_to_aetherium
+
+                user_id = str(member.id)
+
+                if user_id not in dicts["user_currencies"]:
+                    dicts["user_currencies"][user_id] = {}
+                    for user_currency in CURRENCIES_FILE_USE:
+                        dicts["user_currencies"][user_id][user_currency] = 0
+
+                dicts["user_currencies"][user_id]["celesti"] += celesti_reward
+
 
 # --- Events ---
 
@@ -74,6 +150,7 @@ def save_all_files():
 async def on_ready():
     print(f"Eingeloggt als {bot.user} (ID: {bot.user.id})")
     load_all_files()
+    bot.loop.create_task(currency_update_loop())
     print("Bot ist einsatzbereit")
     print("-----")
 
@@ -100,9 +177,10 @@ async def fill(ctx, amnt: int):
         await ctx.send("Du musst eine Anzahl angeben.")
         return
 
-    dicts["pot"]["value"] += amnt
+    dicts["pot"]["solari"] += amnt
+    dicts["currency_totals"]["solari"]
 
-    await ctx.send(f"Der Pot wurde mit {amnt} Solari gefüllt und beträgt nun {dicts["pot"]["value"]}.")
+    await ctx.send(f"Der Pot wurde mit {amnt} Solari gefüllt und beträgt nun {dicts["pot"]["solari"]}.")
 
     save_all_files()
 
@@ -135,9 +213,12 @@ async def give(ctx, user: discord.Member, crncy: str, amnt: int):
             dicts["user_currencies"][user_id][user_currency] = 0
 
     dicts["user_currencies"][user_id][currency] += amnt
+    dicts["currency_totals"][currency] += amnt
     save_all_files()
     await ctx.send(f"{user.display_name} hat {amnt} {CURRENCIES_DISPLAY[currency]} erhalten und hat nun {dicts["user_currencies"][user_id][currency]} {CURRENCIES_DISPLAY[currency]}.")
 
+# DIESEN BEFEHL NICHT NUTZEN! ER VERÄNDERT NICHT DIE TOTALS!
+# warum ist er dann drinne? keine ahnung.
 @bot.command(name="reset", description="Setzt eine Währung eines Users zurück.")
 @commands.check(lambda ctx: ctx.author.id in BOT_ADMINS)
 async def give(ctx, user: discord.Member, crncy: str):
@@ -183,7 +264,7 @@ async def ping(ctx):
 
 @bot.command(name="pot", description="Zeigt den aktuellen Pot.", aliases=["p"])
 async def pot(ctx):
-    await ctx.send(f"Der aktuelle Pot liegt bei {dicts["pot"]["value"]}")
+    await ctx.send(f"Der aktuelle Pot liegt bei {dicts["pot"]["solari"]}")
 
 @bot.command(name="balance", description="Zeigt alle Währungen eines Users an.", aliases=["bal"])
 async def balance(ctx, user: discord.Member=None):
@@ -208,16 +289,28 @@ async def balance(ctx, user: discord.Member=None):
 async def bot_help(ctx):
     cmds = ""
     cmds += "## Userbefehle\n"
-    cmds += "`ping` : Zeigt den Bot-Ping an.\n`pot / p` : Zeigt den aktuellen Pot.\n`balance / bal` : Zeigt alle Währungen eines Users an.\n`help / hilfe / h / cmds` : Sendet eine DM mit allen Befehlen.\n"
+    cmds += "`ping` : Zeigt den Bot-Ping an.\n"
+    cmds += "`pot / p` : Zeigt den aktuellen Pot.\n"
+    cmds += "`balance / bal` : Zeigt alle Währungen eines Users an.\n"
+    cmds += "`help / hilfe / h / cmds` : Sendet eine DM mit allen Befehlen.\n"
+    cmds += "`transfer` : Überträgt eine Währung von deinem Konto auf das eines anderen Users.\n"
+
     cmds += "\n## Adminbefehle\n"
-    cmds += "`say` : Lässt den Bot etwas sagen.\n`shutdown` : Schaltet den Bot aus.\n`fill` : Füllt den Pot.\n`give` : Gibt einem User eine Währung.\n`reset` : Setzt eine Währung eines Users zurück."
+    cmds += "`say` : Lässt den Bot etwas sagen.\n"
+    cmds += "`shutdown` : Schaltet den Bot aus.\n"
+    cmds += "`fill` : Füllt den Pot.\n"
+    cmds += "`give` : Gibt einem User eine Währung.\n"
+    cmds += "`reset` : Setzt eine Währung eines Users zurück.\n"
+
+    cmds += "\n## Glücksspielbefehle\n"
+    cmds += "`coinflip / cf` : Spielt eine Runde Coinflip\n"
 
     await ctx.author.send(cmds)
 
 
 # Glücksspielbefehle
 
-@bot.command(name="coinflip", description="Spielt eine Runde Coinflip.", aliases=["coin", "flip", "cf"])
+@bot.command(name="coinflip", description="Spielt eine Runde Coinflip.", aliases=["cf"])
 async def coinflip(ctx, bet: int, choice: str):
     if bet is None:
         await ctx.send("Du musst einen Einsatz angeben.")
